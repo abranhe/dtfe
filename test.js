f60f39ba4d2fa06ebdcb82f3681768bcce682e86
Abraham Hernandez <abraham@abranhe.com>
2018-10-27 22:37:57 -0400

    changing test

--- test.js
+++ test.js
@@ -2,9 +2,9 @@ import test from 'ava';
 import dtfe from './';
 
 test('Test if a file exsit', t => {
-	t.is(dtfe('LICENSE'), true);
+	t.is(dtfe('license'), true);
 	t.is(dtfe('.travis.yml'), true);
 	t.is(dtfe('.editorconfig'), true);
 	t.not(dtfe('package.json'), false);
-	t.not(dtfe('README.md'), false);
+	t.not(dtfe('readme.md'), false);
 });
